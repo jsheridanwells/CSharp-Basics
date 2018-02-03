51f38506c378a5089338bbc1cd9616cc2f7dcc55
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-03 14:21:51 -0600

    sections 1 - 3

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Operators
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            Console.WriteLine(a / b); // 3 results. they need to be cast to a float
+
+            Console.WriteLine((float)a / (float)b);  // 3.333333
+
+            Console.WriteLine(a > b);  // true
+
+            Console.WriteLine(a != b);  // false
+
+            Console.WriteLine(a > b && b == a); 
+
+
+        }
+    }
+}
---
+++ variables-and-constants/variables-and-constands/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace variablesandconstants
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+        }
+    }
+}
