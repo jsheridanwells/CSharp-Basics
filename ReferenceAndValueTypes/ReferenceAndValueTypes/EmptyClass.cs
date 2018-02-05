46495291d4c2cb5af24b2c1236984c41ddae9dcb
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-05 14:33:58 -0600

    reference and value types

---
+++ ReferenceAndValueTypes/ReferenceAndValueTypes/EmptyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ReferenceAndValueTypes
+{
+    public class Person
+    {
+        public int Age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);  // 1, this is a value type
+
+            var myPerson = new Person() { Age = 20 };
+            MakeOld(myPerson);
+            Console.WriteLine(myPerson.Age); // 30, we just copied a reference to the object
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+    }
+}
---
+++ ReferenceAndValueTypes/ReferenceAndValueTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReferenceAndValueTypes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //VALUE TYPES:::
+            // create an int
+            int a = 10;
+            // copy the int
+            int b = a;
+            // increment the int
+            b++;
+            Console.WriteLine(b);  // 11
+            Console.WriteLine(a);  // 10
+            // these are value types, a copy of that value is pasted into a 
+            // new location in memory
+
+            //REFERENCE TYPES:::
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+
+            array2[0] = 10;
+
+            Console.WriteLine(array2[0]);  // 10
+            Console.WriteLine(array1[0]);  // 10 !!!! (array2 has changed array1)
+
+            Console.WriteLine("array1:{0} array2:{1}", array1, array2);  
+
+        }
+    }
+}
