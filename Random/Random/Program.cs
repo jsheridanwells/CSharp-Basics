dcdb086df9317483cd11a52fbe8f1b69ca145cae
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-06 09:53:05 -0600

    random string generator

---
+++ Random/Random/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // generate a random number
+            var random = new Random();
+            for (var i = 0; i < 10; i++) 
+            {
+                Console.WriteLine(random.Next());
+            }
+
+            // generate LENGTH random characters
+            var Length = 10;
+            var randomChar = new Random();
+            var buffer = new char[Length];
+            for (var i = 0; i < Length; i++) 
+            {
+                buffer[i] = (char)('a' + randomChar.Next(0, 26));
+            }
+            Console.WriteLine(new String(buffer));
+        }
+    }
+}
