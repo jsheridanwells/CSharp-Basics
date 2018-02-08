e4ef506ba3f7af3b55922e718c2ab3c3559153ff
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-08 10:45:57 -0600

    notes added about DateTime

---
+++ Timespan/Timespan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Timespan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+
+            Console.WriteLine("Timespan: ", timeSpan);
+            Console.WriteLine("Timepan 1: ", timeSpan1);
+        }
+    }
+}
