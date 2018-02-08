3fa0fd682f905fd2df605e976d031e4f6af01a4c
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-08 13:42:09 -0600

    text examples and notes

---
+++ Text-Examples/Text-Examples/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Text_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Jeremy Wells   ";
+            Console.WriteLine(fullName.Trim()); // takes off whitespace trailing at end
+            Console.WriteLine(fullName.ToUpper()); // JEREMY WELLS
+            Console.WriteLine(fullName.ToLower()); // jeremy wells
+
+            // substrings
+            var index = fullName.IndexOf(" ");
+            var first = fullName.Substring(0, index);
+            var last = fullName.Substring(index);
+            Console.WriteLine(first + last);
+
+            // but #Split() is easier:
+            var names = fullName.Split(" ");
+            Console.WriteLine(names[0] + names[1]);
+
+            // methods to check if value is null
+            if (String.IsNullOrEmpty(null))  // this is a legacy method
+            {
+                Console.WriteLine("No null values, buddy.");
+            }
+
+            if (String.IsNullOrWhiteSpace("   ")) // this one is more versatile.
+            {
+                Console.WriteLine("No white space, buddy.");
+            }
+
+            // converting
+            var ageInput = "25";
+            var age = Convert.ToByte(ageInput);
+
+            var myFloat = 29.95f;
+            var price = myFloat.ToString("C0");  //  converts float to currency, round to $30
+
+        }
+    }
+}
