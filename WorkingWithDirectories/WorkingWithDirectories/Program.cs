617624dc17b51beaed4abf8690a2cc3c9ecde157
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-09 14:57:59 -0600

    created longestWord and wordCount functions

---
+++ WorkingWithDirectories/WorkingWithDirectories/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates /temp directory in current directory
+            Directory.CreateDirectory(@"./temp");
+            // returns string array with all files and all directories in /folder1
+            Directory.GetFiles(@"./temp/folder1", "*", SearchOption.AllDirectories);
+            // show all files using a 
+
+        }
+    }
+}
---
+++ WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+// System.IO contains classes for working with files
+using System.IO;
+
+// File provides static methods for working with files
+// FileInfo provides instance methods
+/*
+Create()
+Copy()
+Delete()
+Exists()
+GetAttributes()
+Move()
+ReadAllText()
+*/
+
+// Directory and DirectoryInfo is the same for directories
+// Path class has directory path info
+
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // everything with File class is static methods
+            // good for performing methods on files without creating new objects
+            // if it is being used alot, OS performs security checks
+            // which will affect performance
+            var path = @"./temp/";
+            File.Copy(path + "myFile.txt", @"./copy/myFile.txt", true);
+            File.Delete(path + "myFile.txt");
+
+            var fileInfo = new FileInfo(path);
+            // this will allow fileInfo methods on path;
+        }
+    }
+}
