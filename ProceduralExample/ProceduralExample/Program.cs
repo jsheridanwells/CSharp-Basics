809daaa73b9842280db7e3ad4e0594875715d167
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-09 12:50:37 -0600

    refactored code

--- ProceduralExample/ProceduralExample/Program.cs
+++ ProceduralExample/ProceduralExample/Program.cs
@@ -9,14 +9,23 @@ namespace ProceduralExample
             Console.WriteLine("What's your name?  I'll reverse it.");
             var name = Console.ReadLine();
 
+            // with ReverseName method, business logic is separate from control flow.
+            var reversed = ReverseName(name);
+
+            Console.WriteLine("Your name reversed is: " + reversed);
+        }
+
+        // break out method, 
+        // first, thing of what data type you are returning
+        // then, what parameters
+        public static string ReverseName(string name)
+        {
             var array = new char[name.Length];
             for (var i = name.Length; i > 0; i--)
             {
                 array[name.Length - i] = name[i - 1];
             }
-            var reversed = new string(array);
-
-            Console.WriteLine("Your name reversed is: " + reversed);
+            return new string(array);
         }
     }
 }
