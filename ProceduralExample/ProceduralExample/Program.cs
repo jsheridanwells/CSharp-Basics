caa112241d4fbac87acccf5c1c2998ec42f99b62
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-09 12:46:02 -0600

    wrote basic example

---
+++ ProceduralExample/ProceduralExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProceduralExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?  I'll reverse it.");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+            var reversed = new string(array);
+
+            Console.WriteLine("Your name reversed is: " + reversed);
+        }
+    }
+}
