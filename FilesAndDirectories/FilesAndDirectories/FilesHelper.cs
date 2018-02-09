12e6ff3e1ce1f07bc2f030cf8c167e445739f6e4
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-09 14:57:36 -0600

    created get longest word and word count methods

---
+++ FilesAndDirectories/FilesAndDirectories/FilesHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+    
+namespace FilesAndDirectories
+{
+    public class FilesHelper
+    {
+        public static int GetNumWords(string path)
+        {
+            var words = File.ReadAllText(path);
+            var wordsArr = words.Split(' ');
+            return wordsArr.Length;
+        }
+
+        public static string GetLongestWord(string path)
+        {
+            var words = File.ReadAllText(path);
+            var wordsArr = words.Split(' ');
+            var longestWord = "";
+            foreach (var word in wordsArr)
+            {
+                if (word.Length > longestWord.Length) {
+                    longestWord = word;
+                }
+            }
+            return longestWord;
+        }
+    }
+}
